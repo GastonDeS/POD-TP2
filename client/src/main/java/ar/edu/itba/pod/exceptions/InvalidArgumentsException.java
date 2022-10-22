08aa1bdbcc4e7e9fdd6911487cb83d715be7ebd5
linbrittany <brlin@itba.edu.ar>
2022-10-22 12:55:32 -0300

    feat: client arguments parser

---
+++ client/src/main/java/ar/edu/itba/pod/exceptions/InvalidArgumentsException.java
@@ -0,0 +1,7 @@
+package ar.edu.itba.pod.exceptions;
+
+public class InvalidArgumentsException extends Exception {
+    public InvalidArgumentsException(String message) {
+        super(message);
+    }
+}
--- run-registry.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cd `pwd`/server/target/tpe2-g10-server-1.0-SNAPSHOT
-chmod -R +x run-registry
-sh ./run-registry
